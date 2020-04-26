f9f6ef0b5acf2e4d7285c9ac246ab421eeab8b47
IshaanDesai45 <46575231+IshaanDesai45@users.noreply.github.com>
2020-04-26 20:31:26 +0530

    remodel

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import React from 'react';
 
-import Join from './component/Join/Join';
-import Chat from './component/Chat/Chat';
+import Chat from './components/Chat/Chat';
+import Join from './components/Join/Join';
 
-const App = ()=>(
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const App = () => {
+  return (
     <Router>
-        <Route path="/" exact component={Join} />
-        <Route path="/chat"  component={Chat} />
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
-)
+  );
+}
 
-export default App
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
+import App from './App';
 
-import App from './App.js'
-
-ReactDom.render(<App/>,document.querySelector('#root'));
-
-//code above is compulsory for all react applications
+ReactDOM.render(<App />, document.getElementById('root'));
